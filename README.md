82b25a9a41ad64433865b9c5175ed9ca1f7e6ce0
hosamsam <hosamelnabawy1996@gmail.com>
2018-03-06 05:29:23 +0200

    feat[list view]: rework on filter and showing list view
    dispense from using `jquery` to manipulate DOM just use it for AJAX calls
    
    connect the list view events with google maps to add more interactions to the page

--- README.md
+++ README.md
@@ -1,13 +1,49 @@
-# nanodegree neighbourhod map
+# Frontend Nanodegree neighbourhood Map
+ 
+ 
+![project hero](./hero.png)
 
-last project in nanodegree program 
 
-uses Knockout as javascript framework and Google maps
-and **wikipedia** api as a third party library
+Overview
+--------
+Using  `Knockout` as javascript framework and Google maps
+and **wikipedia** *API* as a third party library
 
-### Installation
-after downloading the project files
+<br>
+
+Installation
+------------
+
+* for a rapid view access to the website you can click [link](https://hosamsam.github.io/nanodegree-neighbourhod-map/src/index.html)
+    the project hosted o Github Pages.
+ 
+ <br>
+
+* Test the project source code on your local machine
+ 
+ <br>
+
+1. first clone the project
+
 ```bash
-$ npm install # install the project dependencies 
-$ npm start   # start loacal dev server
+$ git clone https://github.com/hosamsam/nanodegree-neighbourhod-map.git
 ```
+
+2. navigate your shell working directory to the project path
+```bash
+$ cd path/to/projet/foldename
+# or just in case you hadn't changed the name
+$ cd nanodegree-neighbourhod-map
+```
+3. install dependencies & run the project
+
+```bash
+$ npm install
+$ npm start # to run dev live reloading with default browser
+```
+
+<br>
+
+License
+-------
+This project is under [MIT LICENSE](./LICENSE)
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-
+"use strict";
+let starterIndex = 0;
 class Place {
     constructor(title, latlng) {
+        this.index = starterIndex++;
         this.title = title;
         this.latlng = latlng;
     }
@@ -13,6 +15,8 @@ class Wiki {
 }
 
 function AppViewModel() {
+    // selectors & global Variables
+    const searchInput = document.getElementById('search-input');
     this.places = [
         new Place('post office', [37.2668765, -122.0164405]),
         new Place('Saint Andrew\'s Episcopal School', [37.2714124, -122.0165007]),
@@ -22,49 +26,63 @@ function AppViewModel() {
         new Place('Fruitivale Ave', [37.2677997, -122.0149752]),
     ];
     this.staticPlaces = this.places.slice();
-    this.searchValue = ko.observable('');
+    this.map = '';
+    // knockout Observables and utils
     this.neighbourAreas = ko.observableArray(this.places);
-    this.isHidden = ko.observable(true);
     this.showLoader = ko.observable(false);
     this.markers = [];
     this.mapError = ko.observable(false);
     this.noMapError = ko.observable(true);
     this.showInfo = ko.observable(false);
-    this.areasUp = ko.observable(true);
+    this.areasUp = ko.observable(false);
     this.searchTitle = ko.observable('placeholder title');
     this.searchBody = ko.observable('placeholder body');
     this.filterValue = ko.observable('');
-    $('.map-areas').slideDown();
+    this.noMatchedData = ko.observable(false);
+    // knockout AppModel methods
+    searchInput.addEventListener('search', (event)=>{
+        //console.log('search clicked');
+        //console.log(event, event.which);
+    });
 
-    this.toggleArea = (up = false, event = null) => {
-        let mapAreas = $('.map-areas');
-        this.areasUp(true);
-        up ? (mapAreas.slideUp()) : (mapAreas.slideDown());
+    this.toggleArea = () => {
+        let toggleState = this.areasUp();
+        if (this.noMatchedData() === true) {
+            this.noMatchedData(false);
+        }
+        this.resetPlaces(toggleState);
+        this.areasUp(!toggleState);
     };
 
-    this.toggleClose = () => {
-        let mapAreas = $('.map-areas');
-        mapAreas.slideToggle()
+    this.resetPlaces = (toggleState) => {
+        if (!toggleState) { // slide down the list view
+            if (this.neighbourAreas().length < this.places.length)
+                this.markers.forEach(marker=>marker.setMap(this.map));
+            this.neighbourAreas(this.places);
+        }
     };
 
     this.findPlace = (d) => {
-        //console.log(AppViewModel.markers, 'search wiki for', d.title);
         let self = this;
         let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + d.title;
         let requestLink = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exsentences=2&explaintext=&exsectionformat=plain&titles=';
+        let placeIndex = this.places.findIndex(place=>place.title === d.title);
         let noDataHandler = () => {
             this.showLoader(false);
             this.searchTitle('content not reached, try again!').searchBody('');
-        }
+        };
+        // add UI interactions on googlemaps and infobox
+        debounce(self.markers[placeIndex]);
         this.showLoader(true);
-        $.get(searchUrl, { datatype: "jsonp", origin: '*' })
+        // fetch data from WIKI API
+        $.get(searchUrl, { datatype: "jsonp", origin: '*' }) // ensure adding datatype for cross origin errosrs
             .done((data) => {
                 let len = data[1].length,
-                    index = Math.floor(Math.random() * len),
+                    index = Math.floor(Math.random() * len), // getting random page name form available Wiki titles
                     title = data[1][index];
                 if (title) {
                     title = title.replace(/\s+/g, '_');
-                    console.group('WIKI URL SEARCH');
+                    console.groupCollapsed('WIKI URL SEARCH');
                     console.log('SearchUrl Wiki data', data);
                     console.log('Title', title);
                     console.groupEnd();
@@ -73,9 +91,10 @@ function AppViewModel() {
                             let page = data.query.pages,
                                 key = Object.keys(page)[0];
                             self.createInfoBox(new Wiki(page[key].title, page[key].extract))
-                            console.group('WIKI SEARCH RESULT');
+                            console.groupCollapsed('WIKI SEARCH RESULT');
                             console.log('DATA', data, '\n\b', page[key].title, page[key].extract);
                             console.groupEnd();
+                            console.log(String('-').repeat(18));
                         }).fail(noDataHandler)
                 } else {
                     noDataHandler();
@@ -83,49 +102,44 @@ function AppViewModel() {
             }).fail(noDataHandler);
     };
 
+    // generate the new text for info text when clicking on list item and google maps
     this.createInfoBox = (wiki) => {
         this.searchTitle(wiki.title +'<p>📖 provided from ( WIKIPEDIA ) </p>').searchBody(wiki.body || 'ooh.. content not available!');
         this.showInfo(true);
         this.showLoader(false);
-        console.log('the wiki', this.searchTitle(), '\ body\n', this.searchBody(), this.showInfo());
+        console.groupCollapsed('the wiki infoBox');
+        console.log(this.searchTitle(), '\n body\n', this.searchBody(), this.showInfo());
+        console.groupEnd();
     };
-    /*
-    function responsible for filtering places the user enter
 
-
-    */
+    // a function responsible for filtering places the user enter
     this.filter = () => {
         let inputValue = this.filterValue();
         console.log('filtered value', inputValue);
         if (inputValue && inputValue.trim()) {
-            this.neighbourAreas.push(...this.places);
             let reg = new RegExp(inputValue, 'gi');
-            let filtered = Array.from(this.places)
-                .filter(place => place.title.search(reg) >= 0)
-                .map(place => new Place(place.title.replace(reg, '<span class="highlight">$&</span>'), [...place.latlng]));
+            let filtered = this.places.filter(place => place.title.search(reg) >= 0);
 
-            this.loadMap(filtered);
-            this.neighbourAreas.destroyAll();
-            this.neighbourAreas.push(...filtered);
-            this.places = this.staticPlaces;
-            //this.showAreas();
+            if (filtered.length > 0) {
+                let hidePins = this.places.filter(place=>filtered.every(f=>f.index!==place.index));
+                hidePins.forEach(place=>{
+                    this.markers[place.index].setMap(null);
+                });
+                this.filterValue('');
+                this.loadMap(filtered);
+                this.neighbourAreas(filtered);
+                this.places = this.staticPlaces;
+                //this.showAreas();
+            } else {
+                this.noMatchedData(true);
+            }
+
         }
     };
 
-    this.trackKey = (val, event) => {
-        if (event.which === 13) {
-            console.log(event);
-            this.filter();
-        }
-    };
-
-    /*
-        a function responsible for initiating google maps
-
-
-    */
+    // a function responsible for initiating google maps
     this.loadMap = (places) => {
-        console.log('load Map', places);
+        console.log('load Map with places', places);
         let self = this;
         google.maps.event.addDomListener(window, 'load', init);
         function init() {
@@ -137,40 +151,42 @@ function AppViewModel() {
                 //styles: [{ "featureType": "all", "elementType": "all", "stylers": [{ "visibility": "on" }] }, { "featureType": "all", "elementType": "labels", "stylers": [{ "visibility": "off" }, { "saturation": "-100" }] }, { "featureType": "all", "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#000000" }, { "lightness": 50 }, { "visibility": "off" }] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "off" }, { "color": "#000000" }, { "lightness": 20 }] }, { "featureType": "all", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#000000" }, { "lightness": 17 }, { "weight": 1.2 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 20 }] }, { "featureType": "landscape", "elementType": "geometry.fill", "stylers": [{ "color": "#4d6059" }] }, { "featureType": "landscape", "elementType": "geometry.stroke", "stylers": [{ "color": "#4d6059" }] }, { "featureType": "landscape.natural", "elementType": "geometry.fill", "stylers": [{ "color": "#4d6059" }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "lightness": 21 }] }, { "featureType": "poi", "elementType": "geometry.fill", "stylers": [{ "color": "#4d6059" }] }, { "featureType": "poi", "elementType": "geometry.stroke", "stylers": [{ "color": "#4d6059" }] }, { "featureType": "road", "elementType": "geometry", "stylers": [{ "visibility": "on" }, { "color": "#7f8d89" }] }, { "featureType": "road", "elementType": "geometry.fill", "stylers": [{ "color": "#7f8d89" }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#7f8d89" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#7f8d89" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 18 }] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [{ "color": "#7f8d89" }] }, { "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [{ "color": "#7f8d89" }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 16 }] }, { "featureType": "road.local", "elementType": "geometry.fill", "stylers": [{ "color": "#7f8d89" }] }, { "featureType": "road.local", "elementType": "geometry.stroke", "stylers": [{ "color": "#7f8d89" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 19 }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#2b3638" }, { "visibility": "on" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#2b3638" }, { "lightness": 17 }] }, { "featureType": "water", "elementType": "geometry.fill", "stylers": [{ "color": "#24282b" }] }, { "featureType": "water", "elementType": "geometry.stroke", "stylers": [{ "color": "#24282b" }] }, { "featureType": "water", "elementType": "labels", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "labels.text", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }]
             };
             let mapElement = document.getElementById('app-map');
-            let map = new google.maps.Map(mapElement, mapOptions);
+            self.map = new google.maps.Map(mapElement, mapOptions);
             function makeMarker(loc, title = 'silicon Valley') {
                 myLat = new google.maps.LatLng(...loc); // Cairo;
                 let marker = new google.maps.Marker({
                     position: myLat,
-                    map: map,
+                    map: self.map,
                     //draggable: true,
                     animation: google.maps.Animation.DROP,
                     title
                 });
                 self.markers.push(marker);
-                marker.addListener('click', (event) => {
-                    debounce();
+                marker.addListener('click', () => {
+                    debounce(marker);
                     self.findPlace(new Place(title,loc));
                 });
-                marker.setMap(map);
-
-                function debounce() {
-                    if (marker.getAnimation() !== null) {
-                        marker.setAnimation(null);
-                    } else {
-                        marker.setAnimation(google.maps.Animation.BOUNCE);
-                        setTimeout(() => {
-                            marker.setAnimation(null);
-                        }, 850)
-                    }
-                }
+                marker.setMap(self.map);
             }
             places.forEach(place => {
                 makeMarker(place.latlng, place.title);
             });
-            //console.log(self.markers, places);
+            console.log(self.markers);
+
         }
     };
+
+    function debounce(marker) {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(() => {
+                marker.setAnimation(null);
+            }, 850)
+        }
+    }
+
     this.handleMapError = () => {
         this.mapError(true);
         this.noMapError(false);
@@ -181,8 +197,8 @@ function AppViewModel() {
 
 const appViewModel = new AppViewModel();
 
-let handleGoogleError = (err) => appViewModel.handleMapError();
-function initmap()  { appViewModel.loadMap(appViewModel.places) };
+let handleGoogleError = () => appViewModel.handleMapError();
+function initmap () { appViewModel.loadMap(appViewModel.places) }
 // Activates knockout.js
 ko.applyBindings(appViewModel);
 
